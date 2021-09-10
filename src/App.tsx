48912f5ba1b1b7955444bb0ff0877772283b71da
Ana Carolina Machado <69262009+anacarolina1002@users.noreply.github.com>
2021-09-10 17:22:39 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { CookiesProvider } from 'react-cookie';
 import { ContextWrapper } from './contexts/State';
 import SearchBar from './components/SearchBar';
+import "typeface-roboto";
 
 //import Carousel from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
@@ -9,6 +10,9 @@ import sustentalize1 from './images/sustentalize1.png';
 import picapau from './images/picapau.jpg';
 import hamster from './images/hamster.jpg';
 import palhaco from './images/palhaco.jpg';
+import blusaAzul from './images/blusaAzul.jpg';
+import casacoAzul from './images/casacoAzul.jpg';
+import vansBordo from './images/vansBordo.jpg';
 import './App.css';
 
 import Carousel from './components/Carousel';
@@ -27,16 +31,16 @@ function App() {
                   HOME
                 </li>
                 <li>
-                  HOME
+                  ROUPAS
                 </li>
                 <li>
-                  HOME
+                  CALÇAS
                 </li>
                 <li>
-                  HOME
+                  SAPATOS
                 </li>
                 <li>
-                  HOME
+                  CONTATO
                 </li>
               </ul>
             </div>
@@ -48,12 +52,105 @@ function App() {
               <img src={hamster} />
               <img src={palhaco} />
             </Carousel> */}
-            <div className="products">
-              
-            </div>
-          </div>
-
-
+              <div className="title">
+                DESTAQUES DA LOJA
+              </div>
+              <div className="subtitle">
+              A Sustentalize surge com um projeto de tornar a vida mais sustentável através da compra de roupas usadas e em bom estado,<br></br> visando reduzir o descarte de roupas compradas em lojas Fast Fashion.
+              </div>
+              <div className="products">
+                <div className="product-1">
+                  <img src={blusaAzul} className="product-1" alt="product-1" style={{width:350, height:250, marginRight:7}}/>
+                <div className="box">
+                <div className="supertitle">
+                    Por R$:10,00
+                  </div>
+                  <div className="undertitle">
+                  BLUSA AZUL COM<br></br>AMARRAÇÕES<br></br>
+                  TAM P/M
+                  </div>
+                </div>
+                </div>
+                <div className="product-2">
+                  <img src={casacoAzul} className="product-2" alt="product-3" style={{width:350, height:250, marginRight:7}}/>
+                  <div className="box">
+                  <div className="supertitle">
+                    Por R$:15,00
+                  </div>
+                  <div className="undertitle">
+                  CASACO AZUL CURTO<br></br>
+                  TAM P/M
+                  </div>
+                  </div>
+                </div>
+                <div className="product-3">
+                  <img src={vansBordo} className="product-3" alt="product-3" style={{width:350, height:250}}/>
+                  <div className="box">
+                  <div className="supertitle">
+                    Por R$:20,00
+                  </div>
+                  <div className="undertitle">
+                  RÉPLICA TÊNIS VANS<br></br> OLD SCHOOL<br></br>BORDÔ<br></br>
+                  TAM 37
+                  </div>
+                </div>
+                </div>
+                </div>
+                <div className="products">
+                <div className="product-4">
+                  <img src={blusaAzul} className="product-4" alt="product-4" style={{width:350, height:250, marginRight:7}}/>
+                <div className="box">
+                <div className="supertitle">
+                    Por R$:10,00
+                  </div>
+                  <div className="undertitle">
+                  BLUSA AZUL COM<br></br>AMARRAÇÕES<br></br>
+                  TAM P/M
+                  </div>
+                </div>
+                </div>
+                <div className="product-5">
+                  <img src={casacoAzul} className="product-5" alt="product-5" style={{width:350, height:250, marginRight:7}}/>
+                  <div className="box">
+                  <div className="supertitle">
+                    Por R$:15,00
+                  </div>
+                  <div className="undertitle">
+                  CASACO AZUL CURTO<br></br>
+                  TAM P/M
+                  </div>
+                  </div>
+                </div>
+                <div className="product-6">
+                  <img src={vansBordo} className="product-6" alt="product-6" style={{width:350, height:250}}/>
+                  <div className="box">
+                  <div className="supertitle">
+                    Por R$:20,00
+                  </div>
+                  <div className="undertitle">
+                  RÉPLICA TÊNIS VANS<br></br> OLD SCHOOL<br></br>BORDÔ<br></br>
+                  TAM 37
+                  </div>
+                </div>
+                </div>
+              </div>
+              </div>
+              <div className="footer-1">
+                <div className="footer-1-text">
+                Que tal levar a sustentabilidade<br></br>pra dentro de sua casa?
+                </div>
+                <div className="footer-1-subtext">
+                ana.111811@alunosatc.edu.br<br></br>
+                (48) 99651-6580<br></br>
+                Rua Marechal Floriano Peixoto, Criciúma, Santa Catarina - 88801-040<br></br>
+                Atendimento Online
+                </div>
+              </div>
+              <div className="footer-2">
+                Produzido por Ana Carolina Machado e Joel F. Da Silva
+                <span style={{height: 64, width: 64, marginLeft: 0, marginRight: 'auto', marginTop: 0, backgroundImage: 'none', right: 20, bottom: 20, display: 'block'}} className="u-back-to-top u-custom-color-1 u-icon u-icon-circle u-opacity u-opacity-85 u-spacing-15">
+                </span>
+              </div>
         </div>
       </ContextWrapper>
     </CookiesProvider>
